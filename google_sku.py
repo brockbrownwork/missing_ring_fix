e7333486fbb5b181c4b2797ae58923610b6a94d4
Brock Brown <brockrbrown@gmail.com>
2023-09-26 10:43:09 -0400

    Added error handling in case of rate limiting.

--- google_sku.py
+++ google_sku.py
@@ -2,26 +2,30 @@ from urllib.parse import urlparse
 from duckduckgo_search import DDGS
 
 def google_sku(sku):
-    results = []
-    sku = sku.lstrip('0')
-    with DDGS() as search:
-        for result in search.text(sku):
-            results.append(result)
-    possible_urls = []
-    urls = [result["href"] for result in results]
-    domains = [urlparse(url).netloc.split(".")[-2] for url in urls]
-    for i, domain in enumerate(domains):
-        if domain in valid_domains:
-            possible_urls.append(urls[i])
-    print("urls:", urls)
-    print("domains:", domains)
-    print("matching urls:", possible_urls)
-    for url in possible_urls:
-        if url.split("-")[-1] == sku:
-            print(f"Found {sku} at {url}!")
-            return url
-    print(f"Couldn't find a url for {sku} on DuckDuckGo.")
-    return None
+    try:
+        results = []
+        sku = sku.lstrip('0')
+        with DDGS() as search:
+            for result in search.text(sku):
+                results.append(result)
+        possible_urls = []
+        urls = [result["href"] for result in results]
+        domains = [urlparse(url).netloc.split(".")[-2] for url in urls]
+        for i, domain in enumerate(domains):
+            if domain in valid_domains:
+                possible_urls.append(urls[i])
+        print("urls:", urls)
+        print("domains:", domains)
+        print("matching urls:", possible_urls)
+        for url in possible_urls:
+            if url.split("-")[-1] == sku:
+                print(f"Found {sku} at {url}!")
+                return url
+        print(f"Couldn't find a url for {sku} on DuckDuckGo.")
+        return None
+    except Exception as e:
+        print("Got an error from google_sku, are you being rate limited?", e)
+        return None
 
 valid_domains = ["zales", "kay", "jared", "zalesoutlet", "kayoutlet", "https://www.peoplesjewellers.com/"]
 
